aa4a48d36525dfb750cb2bb1364b0c6e329995d8
舟橋　匡 <funafuna63416341@gmail.com>
2021-11-04 15:17:11 +0900

    商品出品機能、STEP1 、手順7まで

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
     
+name       {Faker::Team.name}
+description {'あいう'}
+category_id {'レディース'}
+detail_id      {'新品・未使用'}
+delivery_fee_id {'着払い(購入者負担)'}
+prefectures_id  {'北海道'}
+days_id         {'１~2日で発送'}
+price           {'300'}
+
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  
+
+
+describe '商品出品' do
+  context '商品出品できる場合' do
+    it '全ての値が存在すれば作成できる' do
+      
+    end
+  end
+
+  context '商品できない場合' do
+    it 'nameが空では作成できない' do
+      @item.name = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Name can't be blank")
+    end
+
+    it 'descriptionが空では作成できない' do
+      @item.description = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Description can't be blank")
+    end
+
+    it 'category_idが空では作成できない' do
+      @item.category_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank", "Category can't be blank", "Detail can't be blank", "Delivery fee can't be blank", "Prefectures can't be blank", "Days can't be blank", "Image can't be blank")
+    end
+
+    it 'detail_idが空では作成できない' do
+      @item.detail_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank", "Detail can't be blank", "Detail can't be blank", "Delivery fee can't be blank", "Prefectures can't be blank", "Days can't be blank", "Image can't be blank")
+    end
+
+    it 'delivery_fee_idが空では作成できない' do
+      @item.delivery_fee_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank", "Detail can't be blank", "Delivery fee can't be blank", "Delivery fee can't be blank", "Prefectures can't be blank", "Days can't be blank", "Image can't be blank")
+    end
+    it 'prefectures_idが空では作成できない' do
+      @item.prefectures_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank", "Detail can't be blank", "Delivery fee can't be blank", "Prefectures can't be blank", "Prefectures can't be blank", "Days can't be blank", "Image can't be blank")
+    end
+    it 'days_idが空では作成できない' do
+      @item.days_id = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank", "Detail can't be blank", "Delivery fee can't be blank", "Prefectures can't be blank", "Days can't be blank", "Days can't be blank", "Image can't be blank")
+    end
+    it 'priceが空では作成できない' do
+      @item.price = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
+  end
+end
 end
