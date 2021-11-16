c12b42754720f40bace833beabea4a775653c337
舟橋　匡 <funafuna63416341@gmail.com>
2021-11-16 18:34:50 +0900

    商品情報編集機能、修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :show, only: [:edit, :update]
+  before_action :set_item, only: [:edit, :update, :show]
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -19,21 +19,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
-  def edit  
-    if user_signed_in? && current_user.id == @item.user_id
-    elsif user_signed_in? && current_user.id != @item.user_id 
-      render :index
-    else
-      render :new
-      
-     end
+  def edit
+    redirect_to root_path if current_user.id != @item.user_id
   end
 
   def update
-    if@item.update(item_params)
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit
@@ -45,5 +38,9 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :category_id, :detail_id, :delivery_fee_id, :prefectures_id,
                                  :days_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
