757e6f6b1770d7354185227388cbc9b8be5f5b66
舟橋　匡 <funafuna63416341@gmail.com>
2021-11-27 21:42:26 +0900

    商品購入機能、修正、２

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,14 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
 
   def index
     @order_shipping = OrderShipping.new
     @item = Item.find(params[:item_id])
+    if current_user.id == @item.user.id
+      redirect_to root_path
+    elsif @item.order.present?
+      redirect_to root_path
+    end
   end
 
   def new
